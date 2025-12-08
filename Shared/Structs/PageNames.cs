cc7473507f84440c7651be24b426afba67b7cf5c
gtcriswell <gtcriswell@yahoo.com>
2025-12-08 09:44:33 -0500

    Paage structs

---
+++ Shared/Structs/PageNames.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Structs
+{
+    public static class PageNames
+    {
+
+        public const string Home = "/";
+        public const string Orgs = "/Orgs";
+        public const string Search = "/Search";
+        public const string Signup = "/Signup";
+
+    }
+}
